590c9358fe642ad980721f763aad233873886083
Andrew Wanex <awanex2705@gmail.com>
2019-01-07 14:26:14 -0800

    refactor: fix router exceptions

--- routes/satellite.js
+++ routes/satellite.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const rp = require('request-promise');
+const cors = require('cors');
 
 const satelliteRouter = express.Router();
 
+satelliteRouter.use(bodyParser.json());
+
 satelliteRouter.route('/*')
-  .get((req, res, next) => {
+  .get(cors(), (req, res, next) => {
     rp(`https://www.n2yo.com/rest/v1/satellite/${req.url}&apiKey=${process.env.SATELLITE_API_KEY}`)
       .then(apiRes => {
         res.send(apiRes);
--- routes/weather.js
+++ routes/weather.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const rp = require('request-promise');
+const cors = require('cors');
 
 const weatherRouter = express.Router();
 
+weatherRouter.use(bodyParser.json());
+
 weatherRouter.route('/:requestType/:latlng')
-  .get((req, res, next) => {
+  .get(cors(), (req, res, next) => {
     rp(`https://api.darksky.net/${req.params.requestType}/${process.env.WEATHER_API_KEY}/${req.params.latlng}`)
       .then(apiRes => {
         res.send(apiRes);

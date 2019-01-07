629f41e89226abf16bd026fc217ad30d6c8f85d9
Andrew Wanex <awanex2705@gmail.com>
2019-01-07 14:18:36 -0800

    refactor: set cors in main app instead of routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const weatherRouter = require('./routes/weather');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use(`/${api_version}/`, indexRouter);
 app.use(`/${api_version}/weather`, weatherRouter);
 app.use(`/${api_version}/satellite`, satelliteRouter);

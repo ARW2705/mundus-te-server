8f013446f164fe28750e3617dd5ede09c5a51a58
Andrew Wanex <awanex2705@gmail.com>
2019-01-07 17:45:42 -0800

    refactor: update weather router

--- routes/weather.js
+++ routes/weather.js
@@ -11,12 +11,9 @@ weatherRouter.use(bodyParser.json());
 
 weatherRouter.route('/:requestType/:latlng')
   .get(cors(), (req, res, next) => {
-    console.log(req.url, req.params.requestType, req.params.latlng);
-    console.log(`https://api.darksky.net/${req.params.requestType}/${process.env.WEATHER_API_KEY}/${req.params.latlng}`);
     rp(`https://api.darksky.net/${req.params.requestType}/${process.env.WEATHER_API_KEY}/${req.params.latlng}`)
       .then(apiRes => {
-        console.log(apiRes);
-        res.json(apiRes);
+        res.send(apiRes);
       })
       .catch(err => next(err));
   });

335534fa231dda72f0362a5a5f8f0d8b13d2eac5
Andrew Wanex <awanex2705@gmail.com>
2019-01-09 17:21:24 -0800

    chore: clean up logs

--- routes/satellite.js
+++ routes/satellite.js
@@ -11,7 +11,6 @@ satelliteRouter.use(bodyParser.json());
 
 satelliteRouter.route('/*')
   .get(cors(), (req, res, next) => {
-    console.log(`https://www.n2yo.com/rest/v1/satellite/${req.url}&apiKey=${process.env.SATELLITE_API_KEY}`);
     rp(`https://www.n2yo.com/rest/v1/satellite/${req.url}&apiKey=${process.env.SATELLITE_API_KEY}`)
       .then(apiRes => {
         res.setHeader('content-type', 'application/json');
